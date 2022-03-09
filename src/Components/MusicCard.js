0f135f289cadb4d7dda88f4228c72215a4f90138
Leonidas <leonidas.drawanz@gmail.com>
2022-03-09 19:20:53 -0300

    requisito 7 finalizado e 8 tambem

--- src/Components/MusicCard.js
+++ src/Components/MusicCard.js
@@ -1,23 +1,70 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Carregando from './Carregando';
 
 export default class MusicCard extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: false,
+      favorite: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  async handleChange({ target }) {
+    const { trackName, previewUrl, trackId } = this.props;
+    console.log(target.checked);
+    console.log(trackName);
+    console.log(previewUrl);
+    console.log(trackId);
+    const obj = { trackName, previewUrl, trackId };
+    if (target.checked) {
+      this.setState({ loading: true, favorite: true });
+      await addSong(obj);
+      this.setState({ loading: false });
+    }
+  }
+
   render() {
-    const { trackName, previewUrl } = this.props;
+    const { loading, favorite } = this.state;
+    const { trackName, previewUrl, trackId } = this.props;
 
     return (
       <section>
-        <p>{ trackName }</p>
-        <audio data-testid="audio-component" src={ previewUrl } controls>
-          <track kind="captions" />
-          O seu navegador não suporta o elemento
-          <code>audio</code>
-        </audio>
+        {loading ? (
+          <Carregando />
+        ) : (
+          <div>
+            <p>{ trackName }</p>
+            <audio data-testid="audio-component" src={ previewUrl } controls>
+              <track kind="captions" />
+              O seu navegador não suporta o elemento
+              <code>audio</code>
+            </audio>
+            <label htmlFor="inputFavorita">
+              Favorita
+              <input
+                name="favorite"
+                id="inputFavorita"
+                data-testid={ `checkbox-music-${trackId}` }
+                type="checkbox"
+                checked={ favorite }
+                onChange={ this.handleChange }
+              />
+            </label>
+          </div>
+        )}
       </section>
     );
   }
 }
 
-MusicCard.propTypes = ({
-  musics: propTypes.object,
+MusicCard.propTypes = ({ // aprendido em https://pt-br.reactjs.org/docs/typechecking-with-proptypes.html
+  trackName: propTypes.string,
+  previewUrl: propTypes.string,
+  trackId: propTypes.string,
 }).isRequired;
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import Header from '../Components/Header';
 import getMusics from '../services/musicsAPI';
 import MusicCard from '../Components/MusicCard';
@@ -13,28 +13,20 @@ export default class Album extends Component {
     };
 
     this.musics = this.musics.bind(this);
-    this.teste = this.teste.bind(this);
   }
 
   componentDidMount() {
-    this.teste();
-  }
-
-  async teste() {
-    const musics = await this.musics();
-    const removeFirstItemMusics = musics.shift();
-    console.log(removeFirstItemMusics);
-    if (musics[0].artistName) {
-      this.setState({
-        musics,
-      });
-    }
+    this.musics();
   }
 
   async musics() {
     const { match: { params: { id } } } = this.props;
     const requestedMusics = await getMusics(id);
-    return requestedMusics;
+    // const removeFirstItemMusics = requestedMusics.shift();
+    // console.log(removeFirstItemMusics);
+    this.setState({
+      musics: requestedMusics,
+    });
   }
 
   render() {
@@ -48,13 +40,15 @@ export default class Album extends Component {
             <div>
               <p data-testid="artist-name">{ musics[0].artistName }</p>
               <p data-testid="album-name">{ musics[0].collectionName }</p>
-              {musics.map(({ trackName, trackId, previewUrl }) => (
-                <MusicCard
-                  key={ trackId }
-                  trackName={ trackName }
-                  trackId={ trackId }
-                  previewUrl={ previewUrl }
-                />))}
+              {musics.map((music, index) => (
+                index !== 0
+                  && (
+                    <MusicCard
+                      key={ index }
+                      trackName={ music.trackName }
+                      trackId={ music.trackId }
+                      previewUrl={ music.previewUrl }
+                    />)))}
             </div>
           ) }
       </div>
@@ -62,6 +56,10 @@ export default class Album extends Component {
   }
 }
 
-Album.propTypes = ({
-  id: propTypes.string,
+Album.propTypes = ({ // aprendido em https://pt-br.reactjs.org/docs/typechecking-with-proptypes.html
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
 }).isRequired;
