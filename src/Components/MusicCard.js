8859f1c39b2a77c9249017a737a6dcadad4ecb0d
Leonidas <leonidas.drawanz@gmail.com>
2022-03-10 08:26:35 -0300

    requisito 11

--- src/Components/MusicCard.js
+++ src/Components/MusicCard.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
-import { addSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
+import { addSong, getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 import Carregando from './Carregando';
 
 export default class MusicCard extends Component {
@@ -12,12 +12,32 @@ export default class MusicCard extends Component {
       favorite: false,
     };
 
+    this.handleChange = this.handleChange.bind(this);
     this.favoriteSong = this.favoriteSong.bind(this);
     this.getFavorites = this.getFavorites.bind(this);
+    this.unFavoriteSong = this.unFavoriteSong.bind(this);
   }
 
   componentDidMount() {
     this.getFavorites();
+  }
+
+  async handleChange({ target }) {
+    const { name } = target;
+    const value = target.checked;
+    const { trackName, previewUrl, trackId } = this.props;
+    const obj = { trackName, previewUrl, trackId };
+
+    this.setState({
+      [name]: value,
+    });
+
+    if (value) {
+      this.favoriteSong(obj);
+    } else {
+      this.unFavoriteSong(obj);
+      this.setState({ [name]: value });
+    }
   }
 
   async getFavorites() {
@@ -27,25 +47,22 @@ export default class MusicCard extends Component {
     const { trackId } = this.props;
     console.log(favorites);
     if (favorites.find((item) => item.trackId === trackId)) {
-      console.log('xablau');
       this.setState({ loading: true, favorite: true });
     }
     this.setState({ loading: false });
   }
 
-  async favoriteSong({ target }) {
-    const { trackName, previewUrl, trackId } = this.props;
-    console.log(target.checked);
-    console.log(trackName);
-    console.log(previewUrl);
-    console.log(trackId);
-    const obj = { trackName, previewUrl, trackId };
-    if (target.checked) {
-      this.setState({ loading: true, favorite: true });
-      await addSong(obj);
-      this.getFavorites();
-      this.setState({ loading: false });
-    }
+  async favoriteSong(object) {
+    this.setState({ loading: true, favorite: true });
+    await addSong(object);
+    this.getFavorites();
+    this.setState({ loading: false });
+  }
+
+  async unFavoriteSong(object) {
+    this.setState({ loading: true, favorite: true });
+    await removeSong(object);
+    this.setState({ loading: false });
   }
 
   render() {
@@ -72,7 +89,7 @@ export default class MusicCard extends Component {
                 data-testid={ `checkbox-music-${trackId}` }
                 type="checkbox"
                 checked={ favorite }
-                onChange={ this.favoriteSong }
+                onChange={ this.handleChange }
               />
             </label>
           </div>
