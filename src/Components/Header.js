34af2132312a3460d13c019f3d63aff254dd7fc3
Leonidas <leonidas.drawanz@gmail.com>
2022-03-27 22:30:16 -0300

    adicionado query 320px no header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
+import Logo from '../images/Logo.png';
+import userImage from '../images/userImage.png';
+// eslint-disable-next-line no-unused-vars
+import HeaderCss from '../Css/HeaderCss.css';
 
 export default class Header extends Component {
   constructor() {
@@ -29,15 +33,20 @@ export default class Header extends Component {
   render() {
     const { user, loading } = this.state;
     return (
-      <header data-testid="header-component">
-        <ul>
-          <Link data-testid="link-to-search" to="/search"><li>Search</li></Link>
-          <Link data-testid="link-to-favorites" to="/favorites"><li>Favorites</li></Link>
-          <Link data-testid="link-to-profile" to="/profile"><li>Profile</li></Link>
-        </ul>
+      <header data-testid="header-component" className="header-component">
+        <div className="container-links">
+          <img src={ Logo } alt="logo" />
+          <Link data-testid="link-to-search" to="/search"><p>Search</p></Link>
+          <Link data-testid="link-to-favorites" to="/favorites"><p>Favorites</p></Link>
+          <Link data-testid="link-to-profile" to="/profile"><p>Profile</p></Link>
+        </div>
         {loading
           ? <p>Carregando...</p>
-          : <p data-testid="header-user-name">{ user }</p>}
+          : (
+            <div className="container-user">
+              <img src={ userImage } alt="userImage" />
+              <p data-testid="header-user-name">{ user }</p>
+            </div>)}
       </header>
     );
   }
